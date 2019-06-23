7ac2a71142a2cf31d56c680e33188566492b74d0
Алина Овчинникова <alina665544@gmail.com>
2019-06-23 16:54:29 +0500

    изменение

--- task6UP/Program.cs
+++ task6UP/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace task6UP
 {
@@ -10,13 +8,14 @@ namespace task6UP
     {
         static void Main(string[] args)
         {
-            int a1, a2, a3, M, N = 0, counter = 0, jj = 0, ak = 0;
+            float a1, a2, a3, M;
+            int N = 0, counter = 0, jj = 0;
+            float ak = 0;
             bool exit = false;
             Console.WriteLine("Введите N: ");
-            while (!exit)
-            {
-                N = wwww();
-                if (N < 3) { Console.WriteLine("Миниальная длинна послдовательности 3, повторите ввод"); }
+            while (!exit){
+                N = wwww1();
+                if (N < 3) { Console.WriteLine("Минимальная длинна последовательности 3, повторите ввод"); }
                 else { exit = true; }
             }
             Console.WriteLine("Введите а1: ");
@@ -27,36 +26,41 @@ namespace task6UP
             a3 = wwww();
             Console.WriteLine("Введите M: ");
             M = wwww();
-            List<int> list = new List<int>();
+            List<float> list = new List<float>();
             list.Add(a1);
             list.Add(a2);
             list.Add(a3);
-
-            while (list.Count < N)
-            {
-                ak = list.ElementAt(list.Count - 3) + ((list.ElementAt(list.Count - 2)) / 3) + (3 * list.ElementAt(list.Count - 1));
+            while (list.Count < N){
+                ak = list.ElementAt(list.Count - 1) + ((list.ElementAt(list.Count - 2)) / 3) + (3 * list.ElementAt(list.Count - 3));
                 list.Add(ak);
             }
             Console.WriteLine("Вывод последовательности: ");
-            foreach (int s in list)
+            foreach (float s in list)
                 Console.Write(s + " ");
             Console.WriteLine("\nНомера элементов равных M: ");
-            foreach (int g in list)
-            {
+            string d = M.ToString("N2");
+            foreach (float g in list){
                 jj++;
-                if (g == M) { counter++; Console.Write(jj); }
+                string dd = g.ToString("N2");
+                if (d.Equals(dd)) { counter++; Console.Write(jj); }
             }
             if (counter == 0) { Console.WriteLine("Нет таких элементов"); }
-            else
-            {
-                Console.WriteLine("\nКоличество таких элементов: " + counter);
-            }
+            else{Console.WriteLine("\nКоличество таких элементов: " + counter);}
             Console.ReadKey();
         }
-        static int wwww()
+        static float wwww()
         {
-            while (true)
-            {
+            while (true){
+                float number;
+                if (float.TryParse(Console.ReadLine(), out number))
+                    return number;
+                else
+                    Console.WriteLine("Ошибка, введите еще раз");
+            }
+        }
+        static int wwww1()
+        {
+            while (true){
                 int number;
                 if (int.TryParse(Console.ReadLine(), out number))
                     return number;
